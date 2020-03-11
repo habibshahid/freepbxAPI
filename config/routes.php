867c7f8d080f0f39794914c249b8028bc5235c74
Habib <habib@contegris.com>
2020-03-11 21:43:48 +0500

    - Inbound Routes addition is added   - with Fax Enabled   - with Superfecta settings   - with Call recordings

--- config/routes.php
+++ config/routes.php
@@ -16,12 +16,17 @@ $app->delete('/api/v1', 'ApiController:logout');
 //SIP Extensions
 $app->get('/api/v1/sipExtensions', 'FreePbxController:getAllSIPExtensions');
 $app->post('/api/v1/sipExtension', 'FreePbxController:createSIPExtension');
-$app->put('/api/v1/sipExtension/{id}', 'FreePbxController:updateSIPExtension');
-$app->delete('/api/v1/sipExtension/{id}', 'FreePbxController:deleteSIPExtension');
+$app->put('/api/v1/sipExtension/{id}', 'FreePbxController:updateSIPExtension'); //todo
+$app->delete('/api/v1/sipExtension/{id}', 'FreePbxController:deleteSIPExtension'); //todo
 
 //SIP Trunks
 $app->get('/api/v1/sipTrunks', 'FreePbxController:getAllSIPTrunks');
 $app->post('/api/v1/sipTrunk', 'FreePbxController:createSIPTrunk');
-$app->put('/api/v1/sipTrunk/{id}', 'FreePbxController:updateSIPTrunk');
-$app->delete('/api/v1/sipTrunk/{id}', 'FreePbxController:deleteSIPTrunk');
+$app->put('/api/v1/sipTrunk/{id}', 'FreePbxController:updateSIPTrunk'); //todo
+$app->delete('/api/v1/sipTrunk/{id}', 'FreePbxController:deleteSIPTrunk'); //todo
 
+//Inbound Routes
+$app->get('/api/v1/inboundRoutes', 'FreePbxController:getAllInboundRoutes');
+$app->post('/api/v1/inboundRoute', 'FreePbxController:createInboundRoute');
+$app->put('/api/v1/inboundRoute/{id}', 'FreePbxController:updateInboundRoute'); //todo
+$app->delete('/api/v1/inboundRoute/{id}', 'FreePbxController:deleteInboundRoute'); //todo
